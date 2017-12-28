d3034e2763cf6222323e9037ec31f669f0a80c9c
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2017-12-28 23:17:09 +0300

    программа разбита на функции

--- password_strength.py
+++ password_strength.py
@@ -1,37 +1,46 @@
 import string
 
 
-def get_password_strength(password):
-    password_strength = 10
-    blacklist = load_blacklist_file("blacklist.txt")
-    if len(password) > 6:
-        if not password.count(password[0]) == len(password):
-            if not password.isdigit() and not password.isalpha():
-                if not password.islower() and not password.isupper():
-                    if len(password) > 8:
-                        if not any(year for year in range(1901, 2018) if str(year) in password):
-                            if not any(word for word in blacklist if word in password):
-                                if any(char in string.punctuation for char in password):
-                                    if len(password) > 9:
-                                        return password_strength
-                                    else:
-                                        return password_strength - 1
-                                else:
-                                    return password_strength - 2
-                            else:
-                                return password_strength - 3
-                        else:
-                            return password_strength - 4
-                    else:
-                        return password_strength - 5
-                else:
-                    return password_strength - 6
-            else:
-                return password_strength - 7
-        else:
-            return password_strength - 8
-    else:
-        return password_strength - 9
+def check_pass_length_more_than(password, required_length):
+    if len(password) < required_length:
+        global password_strength
+        password_strength -= 1
+
+
+def check_all_chars_are_not_the_same(password):
+    if password.count(password[0]) == len(password):
+        global password_strength
+        password_strength -= 1
+
+
+def check_all_chars_are_not_the_same_type(password):
+    if password.isdigit() or password.isalpha():
+        global password_strength
+        password_strength -= 1
+
+
+def check_all_chars_are_not_the_same_case(password):
+    if password.islower() or password.isupper():
+        global password_strength
+        password_strength -= 1
+
+
+def check_years_not_in_pass(password):
+    if any(year for year in range(1901, 2018) if str(year) in password):
+        global password_strength
+        password_strength -= 1
+
+
+def check_pass_not_in_blacklist(password, blacklist):
+    if any(word for word in blacklist if word in password):
+        global password_strength
+        password_strength -= 1
+
+
+def ckeck_pass_have_special_chars(password):
+    if not any(char in string.punctuation for char in password):
+        global password_strength
+        password_strength -= 1
 
 
 def load_blacklist_file(filepath):
@@ -41,9 +50,22 @@ def load_blacklist_file(filepath):
     return blacklist
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    password_strength = 10
+    blacklist = load_blacklist_file("blacklist.txt")
+
     print("enter your password:")
     password = input().strip(" ")
-    print("your password strength form 1 to 10 ->> " + str(get_password_strength(password)))
+
+    check_pass_length_more_than(password, 7)
+    check_all_chars_are_not_the_same(password)
+    check_all_chars_are_not_the_same_type(password)
+    check_all_chars_are_not_the_same_case(password)
+    check_pass_length_more_than(password, 9)
+    check_years_not_in_pass(password)
+    check_pass_not_in_blacklist(password, blacklist)
+    ckeck_pass_have_special_chars(password)
+    check_pass_length_more_than(password, 10)
+    print("your password strength ->> {}".format(password_strength))
 
 
